fcad4d60ba83c7b4cd78f333d763da6eb65f4c8e
Anguel M. Metodiev <anguelmetodiev@gmail.com>
2022-11-29 19:20:56 -0500

    Logs Model and MongoDB

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.set("view engine", "jsx");
 app.engine("jsx", reactViews.createEngine());
 app.use(express.urlencoded({ extended: false }));
 
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+});
+
 // New
 
 app.get("/logs/new", (req, res)=>{
